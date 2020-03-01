be5611365240b49b6a2cb9f96a9744a619bc2ba7
sonmbulo <sonmbulo@gmail.com>
2020-03-01 15:21:14 +0800

    add advanced property in ArraySet

--- ArraySet.java
+++ ArraySet.java
@@ -1,5 +1,7 @@
+import java.util.Iterator;
+
 /* A simple array-based set */
-public class ArraySet<T> {
+public class ArraySet<T> implements Iterable<T> {
     private T[] arraySet;
     private int size;
 
@@ -20,6 +22,9 @@ public class ArraySet<T> {
 
     /* add x to the set */
     public void add(T x) {
+        if (x == null) {
+            throw new IllegalArgumentException("can't add null");
+        }
         if (!contains(x)) {
             arraySet[size] = x;
             size += 1;
@@ -29,6 +34,45 @@ public class ArraySet<T> {
     public int size() {
         return size;
     }
+
+    private class setIterator implements Iterator<T> {
+
+        int index;
+
+        public setIterator() {
+            index = 0;
+        }
+
+        /* return #t if there exists next element, #f otherwise */
+        public boolean hasNext() {
+            return index < size;
+        }
+
+        public T next() {
+            T res = arraySet[index];
+            index += 1;
+            return res;
+        }
+    }
+
+    public setIterator iterator() {
+        return new setIterator();
+    }
+
+    public static void main(String[] args) {
+        ArraySet<Integer> intSet = new ArraySet<>();
+        intSet.add(1);
+        intSet.add(2);
+        intSet.add(3);
+
+        /* should print 1 2 3 */
+        for (int x : intSet) {
+            System.out.print(x);
+        }
+    }
 }
 
- 
+
+
+
+
